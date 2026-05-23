295f7612ea1724bcf41dc8a081e4be2e7833656a
aditya <adityalawande17@gmail.com>
2026-05-24 02:32:26 +0530

    Add Vercel Analytics Dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
