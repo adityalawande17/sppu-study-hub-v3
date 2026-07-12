8da00f023ae8bac6dc83a515a2cae9b758b70b7a
adityalawande17 <adityalawande1771@gmail.com>
2026-07-12 19:15:16 +0530

    add: sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+import sitemap from 'vite-plugin-sitemap'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://sppustudyhub.in',
+      dynamicRoutes: [
+        '/tools',
+        '/news',
+        '/first-year',
+        '/branches',
+        '/branches/cs',
+        '/branches/it',
+        '/branches/aids',
+        '/branches/me',
+        '/branches/ce',
+        '/branches/ee',
+        '/branches/etc',
+        '/contribute',
+        '/saved',
+        '/about',
+        '/contact',
+      ]
+    })
+  ]
+})
