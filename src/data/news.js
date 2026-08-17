9e5ae6afe68d0abe78fc3aa1e00abe8126141afc
adityalawande17 <adityalawande1771@gmail.com>
2026-08-18 00:47:15 +0530

    updated news section with exam news

--- src/data/news.js
+++ src/data/news.js
@@ -3,15 +3,23 @@
 export const newsItems = [
   {
     id: 1,
-    title: "B.E 2019 Pattern Time Table Released",
-    date: "2026-05-04",
+    title: "B.E 2019 Pattern Insem Time Table Released",
+    date: "2026-08-18",
     category: "exam",
-    link: "http://collegecirculars.unipune.ac.in/sites/examdocs/Time%20Tables%20APRMAY%202026/B.E.%20(2019%20PATTERN)%20ENDSEM%20EXAM%20TIMETABLE%20FOR%20SUMMER%20SESSION-2026%20(APRIL-MAY-2026).pdf",
+    link: "https://pub-b9de98a8d2b547bf8e0d30a11e336404.r2.dev/sppustudyhub_bucket/2019/B.E.%20INSEM%20EXAM.(2019%20Pattern)%20TIME-TABLE%20FOR%20OCT-NOV-2026%20(Winter%20Session)%20(1).pdf",
     description:
-      "Hope you all had great farewell parties. Now its time to study...",
+      "The Savitribai Phule Pune University (SPPU) has published official Bachelor of Engineering (B.E. 2019 Pattern) In-Semester (Insem) exam schedules on its official circular portal. Check the PDF for more details",
   },
   {
     id: 2,
+    title: "B.E 2019 Pattern Endsem Time Table Released",
+    date: "2026-05-04",
+    category: "exam",
+    link: "http://collegecirculars.unipune.ac.in/sites/examdocs/Time%20Tables%20APRMAY%202026/B.E.%20(2019%20PATTERN)%20ENDSEM%20EXAM%20TIMETABLE%20FOR%20SUMMER%20SESSION-2026%20(APRIL-MAY-2026).pdf",
+    description: "",
+  },
+  {
+    id: 3,
     title:
       "Updated IT Engineering Branch with solved previous year question papers",
     date: "2026-05-1",
@@ -21,7 +29,7 @@ export const newsItems = [
       "Section might also contain question papers from 2019 pattern, as the syllabus is same you can refer them as well",
   },
   {
-    id: 3,
+    id: 4,
     title: "Updated First year Section with previous year question papers",
     date: "2026-03-15",
     category: "new",
